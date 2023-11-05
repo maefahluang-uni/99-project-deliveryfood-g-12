239abdd0efa0e02c07bf165bf3cfc93887f202c4
Chann Myae Zaw <6531503136@lamduan.mfu.ac.th>
2023-11-05 13:49:43 +0700

    ma thi

--- src/main/java/th/mfu/Domain/OrderItem.java
+++ src/main/java/th/mfu/Domain/OrderItem.java
@@ -11,5 +11,24 @@ public class OrderItem {
     private long id;
     private String name;
     private int quantity;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
     
 }
