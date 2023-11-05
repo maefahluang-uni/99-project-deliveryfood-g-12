c6ce81d3ef0ddd71e61b6a7bf321ec79f56723a8
Chann Myae Zaw <6531503136@lamduan.mfu.ac.th>
2023-11-05 13:37:59 +0700

    Order Item done

--- src/main/java/th/mfu/Domain/OrderItem.java
+++ src/main/java/th/mfu/Domain/OrderItem.java
@@ -1,5 +1,15 @@
 package th.mfu.Domain;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int quantity;
     
 }
