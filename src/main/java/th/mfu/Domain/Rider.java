05a7065fde515be307e205a059b2812b2b98f74e
ThetSweLin <6531503189@lamduan.mfu.ac.th>
2023-11-05 11:28:33 +0700

    --

--- src/main/java/th/mfu/Domain/Rider.java
+++ src/main/java/th/mfu/Domain/Rider.java
@@ -1,5 +1,14 @@
 package th.mfu.Domain;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Rider {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
