821f47aaf019a64755a02813573f5a392220e018
Khin Kyawt Shinn <6531503148@lamduan.mfu.ac.th>
2023-11-25 16:10:25 +0700

    ...

--- src/main/java/th/mfu/Domain/custOrder.java
+++ src/main/java/th/mfu/Domain/custOrder.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
-public class custOrder {
+public class CustOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; 
