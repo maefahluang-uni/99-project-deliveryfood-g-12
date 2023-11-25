93a4639cd3c07ffbc1b713639d7bb5ad69e1c9c3
Khin Kyawt Shinn <6531503148@lamduan.mfu.ac.th>
2023-11-25 16:08:21 +0700

    rider

--- src/main/java/th/mfu/Domain/Buyer.java
+++ src/main/java/th/mfu/Domain/Buyer.java
@@ -22,8 +22,24 @@ public class Buyer {
     private String name;
     private String email;
     private String password;
+    private String address;
+    private String phoneNumber;
 
-   
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
     public long getId() {
         return Id;
--- src/main/java/th/mfu/Domain/Rider.java
+++ src/main/java/th/mfu/Domain/Rider.java
@@ -15,7 +15,26 @@ public class Rider {
     private String name;
     private String password;  
     private String email;
+    private String address;
+    private String phoneNumber;
+    
      
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public Long getId() {
         return id;
     }
